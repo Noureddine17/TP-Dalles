da3161f92a1a48a527591ead64bd1ea0fb829a71
Noureddine17 <60039981+Noureddine17@users.noreply.github.com>
2023-01-16 14:40:26 +0100

    Update QCM.cpp

--- tp1-Noureddine17/QCM.cpp
+++ tp1-Noureddine17/QCM.cpp
@@ -1,42 +1,46 @@
 #include <iostream> 
+#include <stdlib.h>
 using namespace std;
 
-int main ()
-{
-	string recommencer;
-	int score ;
-	score = 0;
-	string Reponse;
-	string Reponse2;
 
-	do{
-		cout << "Question 1 - Calculez 100-8 et entrez la reponse: \n A- 92\n B- 108\n C- 44\n D- 108"<<endl;
-		cin >>Reponse;
+int main (){
 
-		if(Reponse == "A") {
-			score += 1;
-			cout <<"Bonne reponse: A- 92 ton score est de "<<score<<" point(s)\n"<<endl;
-		}	 
-		else{ 
-			score+=0;
-			cout << "FAUX (ou verifie les maj) Ton score est de "<<score<<" points\n"<< endl;
+	bool numero[50]={0};
+	bool etoile[11]={0};
+	int tirage[5]={0};
+	int t_e[2]={0};
+	int nbr;
+	srand(time(NULL));
+	
+	for (int i=0;i<5;i++){
+	
+		do{
+			nbr=rand()%50;
 		}
+		while(numero[nbr]==1);	
+		numero[nbr]=1;
+		tirage[i]=nbr+1;
+	}
 
-		cout << "Question 2 - En quel annee la Tour Eiffel a ete cree: \n A- 1887\n B- 1897\n C- 1892\n D- 1901"<<endl;
-		cin >> Reponse2;
-
-		if(Reponse2 == "A") {
-			score += 1;
-			cout <<"Bonne reponse: A- 1887, ton score est de "<<score<<" point(s)"<<endl;
+	for(int x=0; x<5 ;x++)
+	{
+	cout<<"Numero : "<<tirage[x]<<"\n";
+	}
+	
+	for (int i=0;i<2;i++){
+		do{
+		
+			nbr=rand()%11;
 		}
-		else{ 
-			score+=0;
-			cout << "FAUX (ou verifie les maj), ton score est de "<<score<<" points"<< endl;
-		}
-
-		cout << "\nLE QCM EST FINI VEUT TU RECOMMENCER (oui/non)?"<<endl;
-		cin >> recommencer;
-	}while(recommencer == "oui");
-
-return 0;
+		while(etoile[nbr]==1);	
+		etoile[nbr]=1;
+		t_e[i]=nbr+1;
+	}
+	
+	for(int x=0; x<2 ;x++)
+	{
+	cout<<"numero etoiles : "<<t_e[x]<<"\n";
+	}
+		
+		
 }
